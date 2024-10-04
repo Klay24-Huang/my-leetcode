580fdddd83c654dea63052a5b49bf499a4720105
Hao Husiang <HICEIT6@hotaimotor.com.tw>
2024-10-04 16:58:55 +0800

    solve

---
+++ C#/All/2491. Divide Players Into Teams of Equal Skill/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All._2491._Divide_Players_Into_Teams_of_Equal_Skill
+{
+    public class Solution
+    {
+        public long DividePlayers(int[] skill)
+        {
+            Array.Sort(skill);
+
+            long standardValue = skill[0] + skill[^1];
+            long answer = skill[0] * skill[^1];
+            Console.WriteLine(standardValue);
+            for (int i = 1; i < skill.Length/2; i++)
+            {
+                long tempValue = skill[i] + skill[skill.Length-1-i];
+                Console.WriteLine(tempValue);
+                if (tempValue != standardValue)
+                {
+                    return -1;
+                }
+                else
+                {
+                    answer += skill[i] * skill[skill.Length - 1 - i];
+                }
+            }
+            return answer;
+        }
+    }
+}
